b7d7c5eeb4949972b8e3123059d24c8b47d09c9b
Maine Calabrezi <134637736+MaineCalabrezi13@users.noreply.github.com>
2024-04-13 17:12:30 -0300

    Delete Aula 10.04 directory

--- Aula 10.04/fatorial.py
+++
@@ -1,8 +0,0 @@
-#Escreva um programa que lido um número, calcule e informe o seu fatorial. 
-
-num = int(input("Digite um número: "))
-fator = num
-for i in range(num - 1, 1, -1):
-    fator = fator * i
-print(fator)
-
