f2989d8fd17bc1ff9e683721e3499778de562d49
Maine Calabrezi <134637736+MaineCalabrezi13@users.noreply.github.com>
2024-04-18 11:50:57 -0300

    Update Maior.py

--- Aula 17.04/Maior.py
+++ Aula 17.04/Maior.py
@@ -1,8 +1,12 @@
 #Desenvolva um script Python que leia 10 números e informe o maior e menor número lido.
 maior=0
-
+menor=9999
 for i in range(1,11):
     Num = int(input("Digite um número: "))
     if maior<Num:
         maior=Num
+    if menor>Num:
+        menor=Num
 print("O maior número: ",maior)
+print("O menor número: ",menor)
+
