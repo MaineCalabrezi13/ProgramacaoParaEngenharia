8b9ddbc1d43e1747ba535c48481e910c92931c29
Maine Calabrezi <134637736+MaineCalabrezi13@users.noreply.github.com>
2024-04-25 16:58:05 -0300

    Delete Soma.py

--- Soma.py
+++
@@ -1,7 +0,0 @@
-#Faça um Programa Python que leia uma lista de 5 números inteiros, mostre a soma dos elementos da lista.
-
-soma=[]
-for i in range(0,5):
-    num = int(input("Digite números inteiros: "))
-    soma.append(num)
-print("A soma é: ",sum(soma))
