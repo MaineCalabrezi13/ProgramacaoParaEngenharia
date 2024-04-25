a0e0baf966709965ed5f580839d7cc9496777065
Maine Calabrezi <134637736+MaineCalabrezi13@users.noreply.github.com>
2024-04-25 16:57:19 -0300

    Delete MaiorMenor.py

--- MaiorMenor.py
+++
@@ -1,8 +0,0 @@
-#Faça um Programa Python que leia uma lista com 10 números inteiros, calcule e mostre o maior e menor elemento da lista.
-
-num=[]
-for i in range(0,5):
-    numero = int(input("Digite numero: "))
-    num.append(numero)
-
-print("O maior valor é: ",max(num),"\nO menor valor é: ",min(num))
